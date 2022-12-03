2a65b199b826ac4b30e3194f374c95c7a97d402a
1313ou <1313ou@gmail.com>
2022-12-03 11:18:22 +0100

    new interface

---
+++ treebolic-mutable/src/main/java/treebolic/model/ImageDecorator.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2022. Bernard Bou
+ */
+
+package treebolic.model;
+
+import treebolic.model.MutableEdge;
+import treebolic.model.MutableNode;
+
+/**
+ * Decorates nodes and images with images
+ */
+public interface ImageDecorator
+{
+	/**
+	 * Set node image decoration
+	 *
+	 * @param node  node
+	 * @param index index
+	 */
+	void setNodeImage(final MutableNode node, int index);
+
+	/**
+	 * Set node tree edge image decoration
+	 *
+	 * @param node  node
+	 * @param index index
+	 */
+	void setTreeEdgeImage(final MutableNode node, int index);
+
+	/**
+	 * Set edge image decoration
+	 *
+	 * @param edge  edge
+	 * @param index index
+	 */
+	void setEdgeImage(final MutableEdge edge, int index);
+}
