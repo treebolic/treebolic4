5ea48db04f074957fd5e2455c9d0095da70e5c8f
1313ou <1313ou@gmail.com>
2023-12-20 16:54:20 +0100

    launch script

---
+++ swing-studio/run.bat
@@ -0,0 +1,4 @@
+@echo off
+set HERE=%~dp0
+set VERSION=4.1-7
+java -jar "%HERE%\swing-studio-%VERSION%-uber.jar"
---
+++ swing-studio/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#
+# Copyright (c) 2023. Bernard Bou
+#
+
+here=`readlink -f $0`
+here=`dirname "${here}"`
+version=4.1-7
+java -jar "${here}/swing-studio-${version}-uber.jar"
